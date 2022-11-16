5346b66c734c5bb080006226eaca2a0779972a7b
Evan <2248392717@qq.com>
2022-11-16 20:53:18 +0200

    add liked button

--- readme.md
+++ readme.md
@@ -1,8 +1,6 @@
 This is the project for course WWW web service.
 
 pages to do :
-// Upload Event Page Form 
-// Like button and Liked List
 // maintainers page: check and delete the event
 // Notification of events
 
--- src/Pages/EventDetailPage/TitleInfo.tsx
+++ src/Pages/EventDetailPage/TitleInfo.tsx
@@ -1,4 +1,5 @@
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
 interface TitleProps {
   title: string
@@ -9,6 +10,7 @@ interface TitleProps {
 export default function TitleInfo (props: TitleProps): JSX.Element {
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const DateStirng = new Date(props.Date).toLocaleDateString(options)
+  const liked = false
   return (
         <div className="flex flex-col w-4/5 mx-auto min-h-40 justify-center border-b-2 border-blue-200 my-2">
             <Typography variant="h6" className='text-red-500 opacity-70 py-2'>
@@ -17,8 +19,10 @@ export default function TitleInfo (props: TitleProps): JSX.Element {
             <Typography variant="h4" className="">
                 {props.title}
             </Typography>
-            <Typography variant="body2" className='py-2'>
+            <Typography variant="body2" className='py-2 justify-between flex'>
                 {props.Location}
+                <Button variant={liked?"contained":"outlined"} color="primary" className="w-1/5">
+                        {liked?"already liked!":"like this event"}</Button>
             </Typography>
         </div>
   )
