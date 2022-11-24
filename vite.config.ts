f70de992305fd325f48389b4736cb21a6e61609d
Evan <2248392717@qq.com>
2022-11-24 23:08:30 +0200

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "url";
+
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+  plugins: [react(), tsconfigPaths(), svgr()],
+
+  server: {
+    port: 1306,
   },
 });
